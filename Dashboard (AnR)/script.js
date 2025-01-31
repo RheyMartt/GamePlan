7b568fed6b2570912a68f829cb56b49b0654e73b
AiahyabU <111488754+Gkcoli@users.noreply.github.com>
2025-02-01 02:44:35 +0800

    new FnR for analysis
    new Fnr and fixed and adjusted some things

--- Dashboard (AnR)/script.js
+++ Dashboard (AnR)/script.js
@@ -3,4 +3,6 @@ function updateDropdown(game) {
     // Update the dropdown button text
     document.getElementById('dropdownButton').innerText = game;
   }
-  
+  function changeCourtImage(imageSrc) {
+    document.getElementById('courtImage').src = imageSrc;
+  }
